a5bea68051625f01dfa173cde2ae534666372fdf
Tim Beckett <timbeckett@Tims-Air.home>
2021-01-26 17:21:29 -0500

    Can add new links, display links list and individual link.

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -32,6 +34,22 @@ app.get("/links", async (req, res) => {
   const links = await Link.find({});
   console.log(links);
   res.render("links/index", { links });
+});
+
+app.get("/links/new", async (req, res) => {
+  res.render("links/new");
+});
+
+app.post("/links", async (req, res) => {
+  const link = new Link(req.body.link);
+  await link.save();
+
+  res.redirect(`/links`);
+});
+
+app.get("/links/:id", async (req, res) => {
+  const link = await Link.findById(req.params.id);
+  res.render("links/link", { link });
 });
 
 app.listen(3000, () => {
